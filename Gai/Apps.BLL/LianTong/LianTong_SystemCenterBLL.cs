dc5ec941ea263f3a72fb8c69953a9f2ef220a350
WangRex <503431920@qq.com>
2017-02-23 14:40:35 +0800

    Add System center money count

--- Gai/Apps.BLL/LianTong/LianTong_SystemCenterBLL.cs
+++ Gai/Apps.BLL/LianTong/LianTong_SystemCenterBLL.cs
@@ -1,13 +1,9 @@
 ﻿using Apps.Common;
 using Apps.DAL.LianTong;
-using Apps.DAL.Spl;
-using Apps.Models;
-using Apps.Models.Spl;
+using Apps.Models.LianTong;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Apps.BLL.LianTong
 {
@@ -17,6 +13,29 @@ namespace Apps.BLL.LianTong
         public LianTong_SystemCenterBLL()
         {
             m_Rep = new LianTong_SystemCenterRepository();
+        }
+
+        //首页合同金额数
+        public Dictionary<string, long> ContratcMoneyCnt()
+        {
+            List<LianTong_SystemCenterModel> reportList = m_Rep.FindList().ToList();
+            Dictionary<string, long> RefList = new Dictionary<string, long>();
+            foreach (LianTong_SystemCenterModel item in reportList)
+            {
+                long contractCost = 0;
+                long equipmentCost = 0;
+                long projectCost = 0;
+                long Profit = 0;
+                contractCost = Convert.ToInt64(item.contractCost);
+                equipmentCost = Convert.ToInt64(item.equipmentCost);
+                projectCost = Convert.ToInt64(item.projectCost);
+                Profit = contractCost - equipmentCost - projectCost;
+                RefList.Add("contractCost", contractCost);
+                RefList.Add("equipmentCost", equipmentCost);
+                RefList.Add("projectCost", projectCost);
+                RefList.Add("Profit", Profit);
+            }
+            return RefList;
         }
 
     }
